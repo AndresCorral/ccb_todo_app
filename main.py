f6729b7514b5e84cfc615b071b955e660eb9c167
AndresCorral <andresco70@hotmail.com>
2025-01-28 19:13:57 -0500

    fixing cors again

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ for router in routers:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ccbtodofront.netlify.app", 
+    allow_origins=["http://ccbtodofront.netlify.app",
+                "https://ccbtodofront.netlify.app", 
                 "http://localhost:8000", 
                 "https://todoapp-register.flutterflow.app"],
     allow_credentials=True,
